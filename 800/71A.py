3537e8ea7dbd1e88b60b23f800c5b632239eb17f
Your Name <you@example.com>
2024-12-12 22:20:55 -0500

    progress for today

--- 800/71A.py
+++ 800/71A.py
@@ -2,4 +2,18 @@
 # the abbreviation will be the first and last letter and in between the number of letters between the first and last letter
 # process should be automated and the output should be the abbreciated too long words and the short words in the same format
 
+n = int(input())
 
+# Read and parse the words into a list
+words = [input().strip() for word in range(n)]
+
+# Final list of words
+results = []
+
+for word in words:
+    if len(word) > 10:
+        #add logic for changing word so that it is the first and last letter and num of letters inbetween
+    elif len(word) < 11:
+        results.append(word)
+    else:
+        #~
