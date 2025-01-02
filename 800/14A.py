9f13fd204bee7dbe5c5ac1f5845f5f219213c70a
Adrian Powell <adrianpowell2004@gmail.com>
2025-01-01 22:40:32 -0500

    14A progress

---
+++ 800/14A.py
@@ -0,0 +1,19 @@
+# Read dimensions
+n, m = map(int, input().split()) 
+
+# Read the grid
+grid = [input().strip for _ in range(n)]
+
+# Initalize the boundaries
+top_boundary = float('inf')
+bottom_boundary = 0
+left_boundary = float('inf')
+right_boundary = 0
+
+# Find the boundaries
+for i in n:
+    for j in m:
+        
+# Extract the rectangle
+
+#Output result
